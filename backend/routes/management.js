2086c2d9c019c2b6c7d6983af844f8b6ef1fdb9e
GiaiHung <91544801+GiaiHung@users.noreply.github.com>
2022-12-22 14:18:35 +0700

    fix filename

--- backend/routes/management.js
+++ backend/routes/management.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { getAdmin } = require('../controllers/management/adminController')
-const { getPerformance } = require('../controllers/management/PerformanceController')
+const { getPerformance } = require('../controllers/management/performanceController')
 
 router.get('/admin', getAdmin)
 router.get('/performance/:id', getPerformance)
